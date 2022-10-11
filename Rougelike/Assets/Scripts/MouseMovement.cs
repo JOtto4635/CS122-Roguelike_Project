75413da1692f6f31b76ca838b4e3c0807fcb1e3e
EthanMlejnek <mlejnekethan@gmail.com>
2022-10-11 16:50:13 -0500

    Latest Game Files
    - Player Movement (mouse)
    - Animations for player ship
    - Projectile script and animation

---
+++ Rougelike/Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+public class MouseMovement : MonoBehaviour
+{
+    public float maxMoveSpeed = 10;
+    public float smoothTime = 0.3f;
+    Vector2 currentVelocity;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, maxMoveSpeed);
+    }
+
+    private void SetAnimatorMovement(Vector2 mousePosition) // For controlling animations when moving
+    {
+        animator.SetFloat("xDir", mousePosition.x);
+        animator.SetFloat("yDir", mousePosition.y);
+        print(animator.GetFloat("xDir"));
+        print(animator.GetFloat("yDir"));
+    }
+}
+*/
+
+public class MouseMovement : MonoBehaviour
+{
+    public float maxMoveSpeed = 10;
+    public float smoothTime = 0.3f;
+    Vector2 currentVelocity;
+    Vector2 mousePosition;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        TakeInput();
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.position = Vector2.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, maxMoveSpeed);
+        SetAnimatorMovement(mousePosition);
+    }
+
+    private void TakeInput()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void SetAnimatorMovement(Vector2 mousePosition) // For controlling animations when moving
+    {
+        animator.SetFloat("xDir", mousePosition.x);
+        animator.SetFloat("yDir", mousePosition.y);
+        print(animator.GetFloat("xDir"));
+    }
+}
---
+++ Rougelike/Assets/Scripts/TestBullets_2.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBullets_2 : MonoBehaviour
+{
+    public GameObject projectile;
+    public float minDamage;
+    public float maxDamage;
+    public float projectileForce;
+
+    private Vector2 direction;
+
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // 0 for left click 1 for right click
+        {
+            GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity);
+            Vector2 direction = Vector2.up;
+            bullet.GetComponent<Rigidbody2D>().velocity = direction * projectileForce;
+
+        }
+    }
+}
