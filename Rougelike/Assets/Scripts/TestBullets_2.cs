9efcdd8f697e7e82276056dfb14b7b52e9a69d20
Rogan Helm <rogan.helm@mnsu.edu>
2022-11-06 21:58:49 -0600

    Fixed sound
    Set pitch to 1 for music and 2 for bullets. Before, they were set to 0 which is why they made no sound.

--- Rougelike/Assets/Scripts/TestBullets_2.cs
+++ Rougelike/Assets/Scripts/TestBullets_2.cs
@@ -23,12 +23,12 @@ public class TestBullets_2 : MonoBehaviour
         
         if (Input.GetMouseButton(0)) // 0 for left click 1 for right click
         {
-            
             if(shotCheck <= 0)
             {
                 GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity);
                 Vector2 direction = Vector2.up;
                 bullet.GetComponent<Rigidbody2D>().velocity = direction * projectileForce;
+                FindObjectOfType<AudioManager>().Play("LaserCannon"); // This line works here but it also works just above this if statement, not sure which is better
                 shotCheck = 1;
                 Destroy(bullet, maxLifetime);  // Destroy the bullets after a certain time on screen
             }                                                         
